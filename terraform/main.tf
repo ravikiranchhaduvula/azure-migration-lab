c4381d5ce8c27f8c1e610404cbfa92d0ba6a5887
Ravi Kiran Chhaduvula <ravikiran.chd@gmail.com>
2026-07-05 18:22:57 +1000

    Update infrastructure main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.7"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 
